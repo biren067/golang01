222a3708875f638b6a6d2598a689c1a7e398b11e
Birendra Kumar <birendra.kumar@impelsys.com>
2024-02-10 02:52:55 +0530

    run init go lang

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type Greeting struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	r := gin.Default()
+
+	// Define route for the /hello endpoint
+	r.GET("/hello", func(c *gin.Context) {
+		// Create a Greeting object
+		greeting := Greeting{
+			Message: "Hello, World!",
+		}
+
+		// Return JSON response
+		c.JSON(200, greeting)
+	})
+
+	// Start the server
+	r.Run(":8080")
+
+}
